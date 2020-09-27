68e26f66e111d290496ba5434ff9d57112aaa539
MiltonFLJr <miltonferlimajr@gmail.com>
2020-09-27 19:48:47 -0300

    Updates

---
+++ precache-manifest.3a6150fc1db5b3adcfb05be95ae47db0.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba85876bc9bf639d7c693baf2f5cd91",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "29252747dfc4d02539a5",
+    "url": "/Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ba1c8fe428520f08206c",
+    "url": "/Portfolio/static/css/main.2a784324.chunk.css"
+  },
+  {
+    "revision": "29252747dfc4d02539a5",
+    "url": "/Portfolio/static/js/2.4cbfcff5.chunk.js"
+  },
+  {
+    "revision": "4d833975c154ea21126df2a4e5867037",
+    "url": "/Portfolio/static/js/2.4cbfcff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1c8fe428520f08206c",
+    "url": "/Portfolio/static/js/main.290e69d1.chunk.js"
+  },
+  {
+    "revision": "ae9cc64e79c5daa6a293",
+    "url": "/Portfolio/static/js/runtime-main.817d9b67.js"
+  },
+  {
+    "revision": "905b3254a664e29cbec13e0101965646",
+    "url": "/Portfolio/static/media/WeatherInfo.905b3254.png"
+  },
+  {
+    "revision": "6c6cda7e741bf700beae299f015020b3",
+    "url": "/Portfolio/static/media/avatar.6c6cda7e.png"
+  },
+  {
+    "revision": "dea09aac7cd18d4a648a039577d07375",
+    "url": "/Portfolio/static/media/bootstrap.dea09aac.png"
+  },
+  {
+    "revision": "532e5f2fc7ebe0842321e4678b45de65",
+    "url": "/Portfolio/static/media/cellphoneicon.532e5f2f.png"
+  },
+  {
+    "revision": "c3ce0c15c4fa916c837e84474bf143d1",
+    "url": "/Portfolio/static/media/css3.c3ce0c15.png"
+  },
+  {
+    "revision": "2ebc95f2d0f6e3b99ad877a7bfea7e16",
+    "url": "/Portfolio/static/media/easybank-preview.2ebc95f2.jpg"
+  },
+  {
+    "revision": "b56bd68f699893d6e6ed62b44660629d",
+    "url": "/Portfolio/static/media/email.b56bd68f.png"
+  },
+  {
+    "revision": "b1d83a9ceef04dcc291f0354c0f7b7c5",
+    "url": "/Portfolio/static/media/fylo-preview.b1d83a9c.jpg"
+  },
+  {
+    "revision": "1aef001594353d9648edd9b553840817",
+    "url": "/Portfolio/static/media/githubicon.1aef0015.png"
+  },
+  {
+    "revision": "98204b56b0dd2cb1208175a617e6c03c",
+    "url": "/Portfolio/static/media/html5.98204b56.png"
+  },
+  {
+    "revision": "08191552f5bb409ab5cb3490a1ca2705",
+    "url": "/Portfolio/static/media/javascript.08191552.png"
+  },
+  {
+    "revision": "450f2b96f7e9a4dd6bd4203e9ae7d6d1",
+    "url": "/Portfolio/static/media/jquery.450f2b96.png"
+  },
+  {
+    "revision": "7b7cfebe4dbc4b76a30fad7d864e8f72",
+    "url": "/Portfolio/static/media/linkedinicon.7b7cfebe.png"
+  },
+  {
+    "revision": "b9c99b30ceff17feea17572e65741902",
+    "url": "/Portfolio/static/media/manage-preview.b9c99b30.jpg"
+  },
+  {
+    "revision": "49cffdd27a82ef4d0feef0cf7ec43631",
+    "url": "/Portfolio/static/media/mysql.49cffdd2.png"
+  },
+  {
+    "revision": "44403f1eece67c892a0c6a6791da5ad4",
+    "url": "/Portfolio/static/media/phplogo.44403f1e.svg"
+  },
+  {
+    "revision": "ce680e2dd40b559f54c2ea998bee2aee",
+    "url": "/Portfolio/static/media/react.ce680e2d.png"
+  },
+  {
+    "revision": "c662adf54a080f82d2af2d60fd5231b6",
+    "url": "/Portfolio/static/media/rockpaperscissor-preview.c662adf5.jpg"
+  },
+  {
+    "revision": "a5b8a7e653da45157d11157b7973b10f",
+    "url": "/Portfolio/static/media/twittericon.a5b8a7e6.png"
+  }
+]);
