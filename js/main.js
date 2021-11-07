06a51c3f7521633cce3e4e54b2d0e2906a39c05b
MiltonFLJr <miltonferlimajr@gmail.com>
2021-11-07 14:52:23 -0300

    Adding the files of the project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function openMenuoptions(){
+  
+  var displayvalue = document.getElementById('navmobilelinks').style.display;
+  
+  if(displayvalue == 'block'){
+
+  document.getElementById('navmobilelinks').style.display = 'none';
+
+  }else{
+
+  document.getElementById('navmobilelinks').style.display = 'block';
+
+  }
+
+}
